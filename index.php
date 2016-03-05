aa6944d2c7fcc852e703a27abe1074034279ba45
jdspiral <jdspiral@gmail.com>
2016-03-05 10:47:08 -0500

    Get shelters

--- index.php
+++ index.php
@@ -8,8 +8,17 @@
 
 
 $conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=sgihmeyddphdwo password= 7H5KEn9R9_OKLEw8_Rom-w0Aje";
-$dbconn4 = pg_connect($conn_string);
+$conn = pg_connect($conn_string);
 
 print_r($dbconn4);
 
-echo 'hello world';
+
+
+$result = pg_query($conn, "SELECT * FROM shelter");
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+else {
+    print_r($result);
+}
