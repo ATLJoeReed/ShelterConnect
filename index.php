20e6221298edf5f39d03cfe0a787333f7e8acf4b
jdspiral <jdspiral@gmail.com>
2016-03-05 10:22:20 -0500

    Connect to db

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
  */
 
 
+$conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=sgihmeyddphdwo password= 7H5KEn9R9_OKLEw8_Rom-w0Aje";
+$dbconn4 = pg_connect($conn_string);
 
+print_r($dbconn4);
 
 echo 'hello world';
