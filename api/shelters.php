2aaff9349c1714cccaa495cd6d16a003244ff685
jdspiral <jdspiral@gmail.com>
2016-03-05 11:42:41 -0500

    Testing db conn

--- api/shelters.php
+++ api/shelters.php
@@ -6,7 +6,7 @@
  * Time: 11:16 AM
  */
 
-var_dump($_ENV['DATABASE_USERNAME']);
+var_dump(getenv('DATABASE_USERNAME'));
 //
 //$conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=".getenv('DATABASE_USERNAME')." password=". getenv('DATABASE_PASSWORD') . "";
 //$conn = pg_connect($conn_string);
