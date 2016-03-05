d3d234347ea8367530b5a005bf06f3d197b91698
jdspiral <jdspiral@gmail.com>
2016-03-05 12:57:06 -0500

    Test shelter data

--- api/shelters.php
+++ api/shelters.php
@@ -11,8 +11,10 @@
 $conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=".getenv('DATABASE_USERNAME')." password=". getenv('DATABASE_PASSWORD') . "";
 $conn = pg_connect($conn_string);
 
+$lat=33.76;
+$long=-84.46;
 
-$results = pg_query($conn, "select location.id, name, lat, long, address1, address2, city, state, zip_code, beds_available from location join shelter on location.id=shelter.location_id where beds_available>0;");
+$results = pg_query($conn, "select * from ret_locations_tbl({$lat}, {$long}, 5)");
 
 $shelters = pg_fetch_all($results);
 
