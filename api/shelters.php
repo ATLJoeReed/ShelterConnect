037e1a789eaaa75d8508e26d96badaa06db1fab5
jdspiral <jdspiral@gmail.com>
2016-03-05 11:41:54 -0500

    Testing db conn

--- api/shelters.php
+++ api/shelters.php
@@ -12,17 +12,17 @@ var_dump($_ENV['DATABASE_USERNAME']);
 //$conn = pg_connect($conn_string);
 
 
-$results = pg_query($conn, "select location.id, name, lat, long, address1, address2, city, state, zip_code, beds_available from location join shelter on location.id=shelter.location_id where beds_available>0;");
-
-$shelters = pg_fetch_all($results);
-echo '<pre>';
-if (!$shelters) {
-    echo "An error occurred.\n";
-    exit;
-}
-else {
-    foreach ($shelters as $shelter) {
-        json_encode($shelter);
-        exit();
-    }
-}
+//$results = pg_query($conn, "select location.id, name, lat, long, address1, address2, city, state, zip_code, beds_available from location join shelter on location.id=shelter.location_id where beds_available>0;");
+//
+//$shelters = pg_fetch_all($results);
+//echo '<pre>';
+//if (!$shelters) {
+//    echo "An error occurred.\n";
+//    exit;
+//}
+//else {
+//    foreach ($shelters as $shelter) {
+//        json_encode($shelter);
+//        exit();
+//    }
+//}
