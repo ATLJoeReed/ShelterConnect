5420681e8befd7d541f6701f022593eb76930d3f
jdspiral <jdspiral@gmail.com>
2016-03-05 11:22:35 -0500

    Add our connection details

---
+++ api/conn.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josh
+ * Date: 3/5/16
+ * Time: 11:17 AM
+ */
+
+$conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=sgihmeyddphdwo password= 7H5KEn9R9_OKLEw8_Rom-w0Aje";
+$conn = pg_connect($conn_string);
