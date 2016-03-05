f562e35c2bfe3591751f79cb0113303f072302cb
jdspiral <jdspiral@gmail.com>
2016-03-05 10:49:00 -0500

    Get shelters

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 $conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=sgihmeyddphdwo password= 7H5KEn9R9_OKLEw8_Rom-w0Aje";
 $conn = pg_connect($conn_string);
 
-print_r($dbconn4);
+//print_r($dbconn4);
 
 
 
