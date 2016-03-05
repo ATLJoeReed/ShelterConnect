8a9b6e5a38e57f4815f308895bb9148e6c8992f6
jdspiral <jdspiral@gmail.com>
2016-03-05 11:06:14 -0500

    Try again to get the shelters

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $conn = pg_connect($conn_string);
 $results = pg_query($conn, "select  name, lat, long, address1, address2, city, state, zip_code, beds_available from location join shelter on location.id=shelter.location_id where beds_available>0;");
 
 $shelters = pg_fetch_all($results);
+echo '<pre>';
 if (!$shelters) {
     echo "An error occurred.\n";
     exit;
