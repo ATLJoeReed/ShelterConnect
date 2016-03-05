a01121bf09f8c40b7ba35f302d6fd39237a21f37
jdspiral <jdspiral@gmail.com>
2016-03-05 15:11:31 -0500

    Edit edit page

--- shelter-edit.php
+++ shelter-edit.php
@@ -7,12 +7,19 @@
 // */
 $id = $_GET['shelterId'];
 
+print_r($id);
+
 $conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=".getenv('DATABASE_USERNAME')." password=". getenv('DATABASE_PASSWORD') . "";
 $conn = pg_connect($conn_string);
 
-$results = pg_query($conn, "select name, beds_total, beds_available, beds_taken, beds_maintainence, phone_number_1, phone_number_2, phone_number_3, address1, address2, zip_code, lat, long from shelter join location on location.id=location_id where shelter.id={id};" );
+$results = pg_query($conn, "select name, beds_total, beds_available, beds_taken, beds_maintainence, phone_number_1, phone_number_2, phone_number_3, address1, address2, zip_code, lat, long from shelter join location on location.id=location_id where shelter.id=".$id );
 
 $shelters = pg_fetch_all($results);
+
+foreach($shelters as $shelter) {
+    print_r($shelter);
+}
+die();
 
 ?>
 <!DOCTYPE html>
