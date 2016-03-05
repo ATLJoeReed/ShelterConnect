d77ae2f66894b15dd3bdfc80d4fb749e6787330d
jdspiral <jdspiral@gmail.com>
2016-03-05 13:57:11 -0500

    Get data for a particular shelter to edit

--- shelter-edit.php
+++ shelter-edit.php
@@ -4,13 +4,13 @@
  * User: josh
  * Date: 3/5/16
  * Time: 12:25 PM
- */
-$id = $_GET['shelterId'];
+// */
+//$id = $_GET['shelterId'];
 
 $conn_string = "host=ec2-54-227-248-123.compute-1.amazonaws.com port=5432 dbname=d7ci554olg4igm user=".getenv('DATABASE_USERNAME')." password=". getenv('DATABASE_PASSWORD') . "";
 $conn = pg_connect($conn_string);
 
-$results = pg_query($conn, "select name, beds_total, beds_available, beds_taken, beds_maintenance, phone_number_1, phone_number_2, phone_number_3, address1, address2, zip_code, lat, long from shelter join location on location.id=location_id where shelter.id=1;" );
+$results = pg_query($conn, "select name, beds_total, beds_available, beds_taken, beds_maintenance, phone_number_1, phone_number_2, phone_number_3, address1, address2, zip_code, lat, long from shelter join location on location.id=location_id;" );
 
 $shelters = pg_fetch_all($results);
 
